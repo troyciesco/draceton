29479fe0aefc08b1c8899886c9df9932ebd67a2f
troyciesco <tmciesco@gmail.com>
2023-03-04 20:40:47 -0500

    try modifying port

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from "./app.module"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3001, () => {
+  await app.listen(process.env.PORT || 3001, () => {
     console.log(`
-🚀 Server ready at: http://localhost:3001/graphql
+🚀 Server ready at: http://localhost:${process.env.PORT || 3001}/graphql
 ⭐️ See sample queries: http://pris.ly/e/ts/graphql-nestjs#using-the-graphql-api
 `)
   })
