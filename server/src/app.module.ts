de9bb57b4b1844c0f9b28fc13d582651bccdd79c
troyciesco <tmciesco@gmail.com>
2023-03-04 20:26:19 -0500

    try modifying cors in main

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,11 +6,9 @@ import { UserResolver } from "@/resolvers/user.resolvers"
 import { TagResolver } from "@/resolvers/tag.resolvers"
 import { join } from "path"
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo"
-import { ConfigModule } from "@nestjs/config"
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), "src/schema.gql"),
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from "./app.module"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors()
   await app.listen(3001, () => {
     console.log(`
 🚀 Server ready at: http://localhost:3001/graphql
